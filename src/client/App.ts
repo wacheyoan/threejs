aa76d5d4c2fd90897c6de642aff3ed6723521d52
Yoan <wacheyoanpro@gmail.com>
2022-03-25 16:10:00 +0100

    add stats + add dat.gui

--- src/client/App.ts
+++ src/client/App.ts
@@ -1,12 +1,21 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat-gui";
 
 export class App {
+
+    rx = 0.01;
+    ry = 0.01;
 
     private constructor(
         private readonly _renderer: THREE.Renderer,
         private readonly _scene: THREE.Scene,
         private readonly _camera: THREE.PerspectiveCamera,
         private readonly _cube: THREE.Mesh,
+        private readonly _orbitControls: OrbitControls,
+        private readonly _stats: Stats,
+        private readonly _gui: GUI
     ) {
         this._scene.add(this._cube);
     }
@@ -24,11 +33,28 @@ export class App {
                 color: 0xff0000
             })
         );
+        const orbitsControls = new OrbitControls(camera,renderer.domElement);
         camera.position.z = 2;
         renderer.setSize(w,h);
         document.body.appendChild(renderer.domElement);
 
-        const res: App = new App(renderer,scene,camera,cube);
+        const stats = Stats();
+        document.body.appendChild(stats.dom);
+
+        const gui = new GUI();
+        const res: App = new App(renderer,scene,camera,cube,orbitsControls,stats,gui);
+
+        const cubeFolder = gui.addFolder("Cube");
+        cubeFolder.add(cube.scale,'x',0.1,10);
+        cubeFolder.add(cube.scale,'y',0.1,10);
+        cubeFolder.add(cube.scale,'z',0.1,10);
+        cubeFolder.open();
+        const rotationFolder = cubeFolder.addFolder("Rotation")
+        rotationFolder.add(res,"rx",-1,1);
+        rotationFolder.add(res,"ry",-1,1);
+        rotationFolder.open();
+
+
         window.addEventListener('resize',res._onWindowResize.bind(res),false);
 
         return res;
@@ -40,9 +66,10 @@ export class App {
 
     private _processFrame(t: number){
         requestAnimationFrame(this._processFrame.bind(this));
-        this._cube.rotation.x += 0.01;
-        this._cube.rotation.z += 0.01;
+        this._cube.rotation.x += this.rx;
+        this._cube.rotation.z += this.ry;
         this._render();
+        this._stats.update();
     }
 
     private _render(){
